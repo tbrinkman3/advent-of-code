e1e55e0e278ed2cbcd75043999e1f173b3c5e0ae
Tyler Brinkman <tbrinkman3@gmail.com>
2021-12-07 17:26:13 -0600

    First star

---
+++ 06/lanternfish.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+let sampleData = fs.readFileSync('sample-input.txt').toString('utf-8').split(',').map(num => Number(num));
+let realData = fs.readFileSync('input.txt').toString('utf-8').split(',').map(num => Number(num));
+
+
+
+const calculateLanternFish = (input, days) => {
+  let day = 0;
+  while(day < days) {
+    let fish = 0;
+    let addNewFish = false;
+
+    while(fish < input.length) {
+      input[fish]--;
+      if (input[fish] === -1) {
+        input[fish] = 6;
+        //compensate for input--
+        input.push(9);
+      }
+      fish++;
+    }
+    day++;
+  }
+  console.log(input)
+  return input.length
+}
+
+console.log(calculateLanternFish(realData, 80))
