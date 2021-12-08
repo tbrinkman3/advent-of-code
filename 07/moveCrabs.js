bb7c2151d308177c9d26ca2302d643578935ee77
Tyler Brinkman <tbrinkman3@gmail.com>
2021-12-08 13:07:01 -0600

    Got second star! Used triangle computation for rate of increase

--- 07/moveCrabs.js
+++ 07/moveCrabs.js
@@ -5,25 +5,58 @@ const realData = fs.readFileSync('data.txt').toString('utf-8').split(',').map(nu
 
 //console.log(sampleData)
 
-const moveCrabs = (input) => {
+const moveCrabsRateConstant = (input) => {
   //get inital cost array
   let diffPoints = Array.from(new Set(input));
 
   let costArray = input.map(point => Math.abs(point - input[0]));
   let cost = costArray.reduce((prev, curr) => {return prev + curr}, 0);
 
+  let currCostExp = 1;
+
+  let costArr = [];
+
   for(let i = 1; i < diffPoints.length; i++) {
-    let newCostArray = costArray.map(point => Math.abs(point - diffPoints[i]));
-    let newCost = newCostArray.slice().reduce((prev, curr) => {return prev + curr}, 0);
-    if(newCost > cost) {
-      break;
-    } else {
+    //find cost at next different horizontal line
+    let newCostArray = costArray.map(point => Math.abs(point - diffPoints[i]) * currCostExp);
+    //calculate new cost
+    let newCost = [...newCostArray].reduce((prev, curr) => {return prev + curr}, 0);
+
+      costArr.push(newCost)
+      currCostExp++;
       cost = newCost
-    }
+
   }
-
-
-  return cost
+  return costArr
 }
 
-console.log(moveCrabs(realData))
+const moveCrabsRateIncrease = (input) => {
+  //get inital cost array
+  let diffPoints = Array.from(new Set(input));
+
+  let costArray = input.map(point => Math.abs(point - input[0]));
+  let cost = costArray.reduce((prev, curr) => {return prev + curr}, 0);
+
+  let currCostExp = 1;
+
+  let costArr = [];
+
+  for(let i = 1; i < diffPoints.length; i++) {
+
+    //find cost at next different horizontal line
+    let newCostArray = costArray.map(point => {
+      let change = Math.abs(point - diffPoints[i])
+      return (change * (change + 1)) / 2
+      });
+    //calculate new cost
+    let newCost = [...newCostArray].reduce((prev, curr) => {return prev + curr}, 0);
+
+      costArr.push(newCost)
+      currCostExp++;
+      cost = newCost
+
+  }
+  return Math.min(...costArr)
+}
+
+console.log(moveCrabsRateIncrease(realData))
