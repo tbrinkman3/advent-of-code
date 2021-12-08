917f2bd4cd8b0e52f0da3cc0dbc615850671da34
Tyler Brinkman <tbrinkman3@gmail.com>
2021-12-08 10:14:34 -0600

    Most common location func

--- 07/moveCrabs.js
+++ 07/moveCrabs.js
@@ -3,3 +3,31 @@ var fs = require('fs');
 const sampleData = fs.readFileSync('sample-data.txt').toString('utf-8').split(',').map(num => Number(num))
 
 console.log(sampleData)
+
+const moveCrabs = (input) => {
+
+  const mostCommonLocation = (locations) => {
+    let mostCommonCount = 0;
+    let mostCommonLoc;
+
+    for(let loc in locations) {
+      if (locations[loc] > mostCommonCount) {
+        mostCommonCount = locations[loc];
+        mostCommonLoc = loc;
+      }
+    }
+
+    return mostCommonLoc;
+  }
+
+  let crabLocations = {};
+
+  for(let crab of input) {
+    crabLocations[crab] ? crabLocations[crab] += 1 : crabLocations[crab] = 1;
+  }
+
+  return mostCommonLocation(crabLocations)
+
+}
+
+console.log(moveCrabs(sampleData))
